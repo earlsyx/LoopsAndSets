1c85588bfaea88ffb0bb6ad2f6e1019c9803992f
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-15 06:44:44 +0800

    do while chekc

---
+++ HWDoLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿
+bool continueAsking = true;
+
+
+do
+{
+    Console.Write("What is your Firstname?");
+    string firstName = Console.ReadLine();
+    string formattedName;
+    if (firstName.ToLower() == "tim")
+    {
+        formattedName = $"Professor {firstName}";
+        Console.WriteLine($"Welcome {formattedName}");
+    }
+    else
+    {
+        Console.WriteLine($"Welcome {firstName}");
+    }
+
+    Console.Write($"Type \"exit\" to stop asking the name ");
+    var inputText = Console.ReadLine();
+    if (inputText.ToLower() == "exit")
+    {
+        continueAsking = false;
+    }
+}
+while (continueAsking);

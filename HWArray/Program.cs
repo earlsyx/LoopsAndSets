ad0a68ea65bc42f7085fc810ec6361b5789f70de
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-15 07:26:07 +0800

    tryparse outofboudns check array string

---
+++ HWArray/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+// array of 3 names
+// ask:<whichnumbertoselect>
+// return the name once user gives the number
+// check for invalid numbers.
+
+string[] names = new string[] { "jayson", "david", "seb" };
+
+Console.Write("Which number do you want to select: ");
+string numberText = Console.ReadLine();
+
+bool isValidNumber = int.TryParse(numberText, out int number);
+
+
+if (!isValidNumber)
+{
+    Console.WriteLine("Invalid number");
+    return;
+}
+
+bool isOutOfBounds =  number > names.Length - 1;
+
+if (isOutOfBounds)
+{
+    Console.WriteLine("Number given is larger the the nubmer of names in the array");
+    return;
+}
+
+Console.WriteLine($"The name based on the number given is {names[number]}");
+

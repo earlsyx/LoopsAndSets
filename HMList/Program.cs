129ccfd73981089d0ca2c827da166cb7092c1b18
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-15 20:42:55 +0800

    list assignment
    adding
    
    try parse

---
+++ HMList/Program.cs
@@ -0,0 +1,30 @@
+﻿
+bool addMoreStudents = true;
+List<string> students = new List<string>();
+
+do
+{
+	Console.WriteLine("Enter Student to be added");
+	Console.Write("Add: ");
+	string studentsText = Console.ReadLine();
+	students.Add(studentsText);
+	Console.Write("Do you want to add more? Yes/No");
+	string answer = Console.ReadLine();
+	if(answer.ToLower() == "yes")
+    {
+        addMoreStudents = true;
+    }
+	else if (answer.ToLower() == "no")
+	{
+		addMoreStudents = false;
+        Console.WriteLine("List of Students:");
+		for (int i = 0; i < students.Count; i++)
+		{
+            Console.WriteLine(students[i]);
+        }
+    }
+	else
+	{
+        Console.WriteLine("Not a valid answer!");
+	}
+} while (addMoreStudents);

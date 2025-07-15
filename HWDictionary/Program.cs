5f42ca2c9f23400140612494e80fe8317b84fd32
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-15 21:16:20 +0800

    Dictonary HW - add value in a dictionary - print dicitionary value.

---
+++ HWDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// dictionary list of employee Ids and name.
+// fill in a few records, ask the user for their ID and return their name.
+
+//Current employee dictionary list
+
+Dictionary<string, string> employee = new Dictionary<string, string>();
+employee["1"] = "Jayson";
+employee["123"] = "David";
+employee["5123"] = "Seb";
+employee["222"] = "Ash";
+
+Console.Write("Enter employee Id");
+string employeeId = Console.ReadLine();
+
+Console.WriteLine($"The employee name is {employee[employeeId]}");
+
